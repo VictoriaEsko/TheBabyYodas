308242178de015ce1ecee4f26107d5d00f6bc0d8
Ebba Ankar <ebbaankar@AirsomtlhorEbba.lan>
2023-01-27 15:04:18 +0100

    import done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import DropDown from "./components/dropDown";
 
 function App() {
 
   return (
     <div className="App">
-   
+   <DropDown/>
     </div>
   )
 }
---
+++ src/components/dropDown.jsx
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from "React";
+
+export default function dropDown() {
+     
+    return (
+      <div></div>
+    );
+  };
+  
