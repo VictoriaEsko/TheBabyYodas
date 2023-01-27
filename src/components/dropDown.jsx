863ae709819856e6cf8aeeaa7c9fb7c27c409298
Ebba Ankar <ebbaankar@AirsomtlhorEbba.lan>
2023-01-27 15:16:21 +0100

    started creating a dropdown menu

--- src/components/dropDown.jsx
+++ src/components/dropDown.jsx
@@ -1,9 +1,35 @@
 import React, {useState, useEffect} from "React";
 
 export default function dropDown() {
-     
-    return (
-      <div></div>
-    );
-  };
+    const [state, setState] = useState(false);
+  
+  //   const toggle = ()=>{
+  //     setState(!state);
+  //   }
+       
+      return (
+          <div onClick={()=>{
+              setState(!state)
+          }} className={`category + ${state ? "category-open" : "category-close"}`}>
+           <h2>Catalog </h2>
+        <div>
+          {/* <button onClick={toggle} className={`toggle-btn + ${state ? "toggle-close" : "toggle-open"}`}>
+          Category
+          </button> */}
+          <div className={`menu-list + ${state ? "menu-open" : "menu-close"}`}>
+              <ul>
+                  <li><a href="">People</a></li>
+                  <li><a href="">Planets</a></li>
+                  <li><a href="">Films</a></li>
+                  <li><a href="">Species</a></li>
+                  <li><a href="">Vehicles</a></li>
+                  <li><a href="">Starships</a></li>
+              </ul>
+          </div>
+      </div>
+      </div>
+    
+      );
+    };
+    console.log("hej hej hej") 
   
