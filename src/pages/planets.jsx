57d59633b665fc7b14795b93a00ae7f6f13f8c7b
arasvall <sara.vall@timecut.se>
2023-02-02 13:38:48 +0100

    added css and planets key

--- src/pages/planets.jsx
+++ src/pages/planets.jsx
@@ -17,16 +17,23 @@ export default function fetchPlanets() {
 
   if (url === "/planets") {
     return (
-      <>
+      <div className="cards">
         {data &&
           data.map((planets) => (
-            <div key={planets.url}>
+            <div key={planets.url} className="card" >
               <p>{planets.name}</p>
               <p>{planets.gravity}</p>
+              <p>{planets.climate}</p>
+              <p>{planets.population}</p>
+              <p>{planets.terrain}</p>
+              <p>{planets.rotation_period}</p>
+              <p>{planets.orbital_period}</p>
+              <p>{planets.surface_water}</p>
+              <p>{planets.diameter}</p>
               <br />
             </div>
           ))}
-      </>
+      </div>
     );
   }
 }
